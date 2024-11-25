9e01edeb26e23cdcd3983631b2526611fbd05fce
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-25 17:01:43 +0100

    Create the Register DTO; g

--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -1,1 +1,22 @@
-export class RegisterDto {}
+import { IsEmail, IsEnum, IsOptional, IsString, MinLength } from "class-validator";
+import { Role } from "src/modules/users/users.schema";
+
+export class RegisterDto {
+    @IsString()
+    @MinLength(4)
+    username: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @MinLength(8)
+    password: string;
+
+    @IsOptional()
+    @IsString()
+    avatar?: string;
+
+    @IsEnum(Role)
+    role: Role;
+}
