52c74e4b56145e8bec94118857f3576e3dcb0eb0
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-25 21:18:08 +0100

    AUTH: Create auth Middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,32 @@
+import { Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      throw new UnauthorizedException(
+        'Missing or invalid authorization header',
+      );
+    }
+
+    const token = authHeader.split(' ')[1];
+    try {
+        const decoded = await this.jwtService.verifyAsync(token, {
+            secret: process.env.JWT_SECRET,
+        });
+        req.user = decoded;
+        next();
+    } catch (err) {
+        if(err.name === 'TokenExpiredError') {
+            throw new UnauthorizedException('Token expired');
+        } 
+        throw new UnauthorizedException('Invalid token');
+    }
+  }
+}
