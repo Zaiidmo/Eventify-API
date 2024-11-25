034dd00e1e5db6fe16806adf9da2214c838436aa
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-25 21:41:29 +0100

    AUTH: Create Roles.Guard and Roles Decorator

---
+++ src/guards/roles.guard.ts
@@ -0,0 +1,32 @@
+import { Role } from '@/modules/users/users.schema';
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.get<Role[]>(
+      'roles',
+      context.getHandler(),
+    );
+    if (!requiredRoles) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    if (!user) {
+      throw new ForbiddenException('User not found');
+    }
+    if (!requiredRoles.includes(user.role)) {
+      throw new ForbiddenException('Insufficient role');
+    }
+    return true;
+  }
+}
