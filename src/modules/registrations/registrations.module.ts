6fa7eb746c078ef2a72297936e68b1f6d36a4692
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-25 16:54:49 +0100

    DATABASE: Importing Schemas into Modules

--- src/modules/registrations/registrations.module.ts
+++ src/modules/registrations/registrations.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RegistrationsService } from './registrations.service';
 import { RegistrationsController } from './registrations.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RegistrationSchema } from './registrations.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Registration', schema: RegistrationSchema }])],
   controllers: [RegistrationsController],
   providers: [RegistrationsService],
 })
