7c89a0e2cef247c7946b001fc6b522635b185ec5
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-27 14:59:41 +0100

    DATABASE: Update events and Registrations schema

--- src/modules/events/events.schema.ts
+++ src/modules/events/events.schema.ts
@@ -1,27 +1,31 @@
-import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
-import { Document } from "mongoose";
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
 
 export type EventDocument = Event & Document;
 
 @Schema({ timestamps: true })
 export class Event {
-    @Prop({ required: true })
-    title: string;
+  @Prop({ required: true })
+  title: string;
 
-    @Prop({ required: true })
-    description: string;
+  @Prop({ required: true })
+  description: string;
 
-    @Prop({ required: true })
-    date: Date;
+  @Prop({ required: true })
+  date: Date;
 
-    @Prop({ required: true })
-    location: string;
+  @Prop({ required: true })
+  location: string;
 
-    @Prop({ required: true })
-    capacity: number;
+  @Prop({ required: true })
+  capacity: number;
 
-    @Prop()
-    banner?: string;
+  @Prop()
+  banner?: string;
+
+  // Organizer Relationship
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  organizer: Types.ObjectId; 
 }
 
 export const EventSchema = SchemaFactory.createForClass(Event);
--- src/modules/registrations/registrations.schema.ts
+++ src/modules/registrations/registrations.schema.ts
@@ -1,16 +1,18 @@
-import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
-import { Document, Types } from "mongoose";
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
 
 export type RegistrationDocument = Registration & Document;
 
 @Schema({ timestamps: true })
 export class Registration {
-    @Prop({ type: Types.ObjectId,ref:'Event', required: true })
-    event: Types.ObjectId;
-    
-    @Prop({ type: [{type: Types.ObjectId, ref:'User'}], required: true })
-    participants: Types.ObjectId[];
- 
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  user: Types.ObjectId;
+
+  @Prop({ type: Types.ObjectId, ref: 'Event', required: true })
+  event: Types.ObjectId;
+
+  @Prop({ default: Date.now })
+  registrationDate: Date;
 }
 
 export const RegistrationSchema = SchemaFactory.createForClass(Registration);
