4758ec45161cd281d2d2ff4dd3f62a84fea2bb23
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-27 10:21:06 +0100

    EVENTS: Create createEventDto

--- src/modules/events/dto/create-event.dto.ts
+++ src/modules/events/dto/create-event.dto.ts
@@ -1,1 +1,42 @@
-export class CreateEventDto {}
+import {
+  IsDate,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MaxLength,
+  minLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateEventDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(50)
+  @IsNotEmpty({ message: 'Event name is required' })
+  name: string;
+
+  @IsNotEmpty({ message: 'Description is required' })
+  @IsString()
+  @MinLength(10, { message: 'Description must be at least 10 characters long' })
+  @MaxLength(1000, { message: 'Description must be less than 1000 characters' })
+  description: string;
+
+  @IsNotEmpty({ message: 'Location is required' })
+  @IsString()
+  location: string;
+
+  @IsNotEmpty({ message: 'Date is required' })
+  @IsDate()
+  date: Date;
+
+  @IsNotEmpty({ message: 'Capacity is required' })
+  @IsInt()
+  @IsPositive()
+  capacity: number;
+
+  @IsString()
+  @IsOptional()
+  banner?: string;
+}
