82e30362dfd9ff7cd03a069b6cc662a8402cba79
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-29 16:23:03 +0100

    Create the upload module files

---
+++ src/upload/providers/upload.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { S3 } from 'aws-sdk';
+import { UploadInterface } from '../interfaces/upload.interface';
+
+@Injectable()
+export class UploadService implements UploadInterface {
+  private s3: S3;
+
+  constructor() {
+    this.s3 = new S3({
+      region: process.env.AWS_S3_REGION,
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
+    });
+  }
+
+  async uploadFile(file: Express.Multer.File): Promise<{ url: string; key: string; }> {
+      const fileExtension = file.originalname.split('.').pop();
+      const uniqueFileName = `${Date.now()}.${fileExtension}`;
+        const params = {
+            Bucket: process.env.AWS_S3_BUCKET_NAME,
+            Key: uniqueFileName,
+            Body: file.buffer,
+            ACL: 'public-read',
+        };
+
+    try { 
+        console.log('Uploading file to S3...');
+        const uploadResult = await this.s3.upload(params).promise();
+        console.log('File uploaded to S3');
+        return {
+            url: uploadResult.Location,
+            key: uploadResult.Key,
+        };
+    } catch (error) {
+        throw new Error(`Error uploading file to S3: ${error.message}`);
+    }
+  }
+}
